8aaa10d8cb445d8370b898779ddc219e2eeecb20
Salar <salar.taeb@hotmail.com>
2021-04-24 13:43:13 -0700

    completing finishing up server code

--- index.js
+++ index.js
@@ -17,4 +17,20 @@ app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id)
+
+  socket.on('disconnet', () => {
+    socket.broadcast.emit("callended");
+  })
+  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("calluser", {signal: signalData, from, name });
+  });
+
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  });
+
+});
+
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
