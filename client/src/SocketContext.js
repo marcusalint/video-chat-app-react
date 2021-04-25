dea490ce5b9cc3afc8c112de2403ef0dc8176f8c
Salar <salar.taeb@hotmail.com>
2021-04-24 19:05:37 -0700

    fixing minor bug

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,8 +4,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-// const socket = io('https://react-video-chat-app-salar.herokuapp.com/');
-const socket = io('http://localhost:5000');
+const socket = io('https://react-video-chat-app-salar.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
